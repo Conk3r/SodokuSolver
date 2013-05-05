a40b795b4d88d8acfea9bf0f743474ee4f2650c3
Eric <Eric_Hansen91@web.de>
2013-05-05 17:10:41 +0200

    1.01 Code changed for better readebility

--- PermutationTest.java
+++ PermutationTest.java
@@ -1,29 +1,32 @@
 
 public class PermutationTest {
-
-	public static boolean isPermutation(int [] array, int arLength ){
+	
+	public final static int BLANK = 0; // To see exactly where the Array Value is meant
+	
+	public static boolean isPermutation(int [] array){
 		
 		int[] check = new int [arLength + 1]; 
 		int i = -1;
+		
 		
 
 /* Check is use to control if a number is used twice in an array. */
 /* Is a number i used check[i] is changed to zero. 				  */
 		
 		
-		for (int k = 0; k <= arLength ;k++)
+		for (int k = 0; k <= array.length ;k++)
 				{
 				check[k] = 1;
 				
 				}
 		
-		while( i < arLength - 1  )
+		while( i < array.length - 1  )
 			{	
 			i++;
 			if (check[array[i]] == 0)	// Number has been used twice => false
 						return false;
 					
-			if (check[array[i]] == 1 && array[i] != 0)
+			if (check[array[i]] == 1 && array[i] != BLANK)
 						check[array[i]] = 0;
 			}
 		return true;
